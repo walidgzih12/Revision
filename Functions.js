a9e1cec79d62983d77db13340ba0307733587b0c
walidgzih12 <walidgzih3@gmail.com>
2023-12-24 14:21:07 +0100

    work on progress

---
+++ Functions.js
@@ -0,0 +1,36 @@
+/*                                  *** Exercice 1 ***
+
+function Or(state1,state2){
+
+    return !(state1 && state2)
+
+}
+
+***invokig***
+
+Or(5>1,6<1)
+***output***
+true
+
+*/
+
+// **********************************************************************************************
+                      
+
+ /*                              *** Exercice 2 ***
+
+ function Opposite (boolean){
+
+    console.log(!boolean)
+ }
+
+***invokig***
+ Opposite(true)
+
+***output***
+ false
+
+*/
+
+
+
