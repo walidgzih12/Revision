649d1373400689d9cfb9992e3599cca65e26e379
walidgzih12 <walidgzih3@gmail.com>
2023-12-24 15:41:11 +0100

    work on progress

--- Functions.js
+++ Functions.js
@@ -7,8 +7,8 @@ function Or(state1,state2){
 }
 
 ***invokig***
-
 Or(5>1,6<1)
+
 ***output***
 true
 
@@ -31,6 +31,75 @@ true
  false
 
 */
+
+
+// **********************************************************************************************
+                      
+
+ /*                              *** Exercice 3 ***
+
+ function greaterThan5 (num1 , num2){
+
+    if(num1 > 5 || num2 > 5){
+        return true
+    
+    }
+
+    else if(num1 < 5 && num2 < 5){
+        return false
+    }
+
+}
+
+***invokig***
+greaterThan5(1, 6) -> true 
+greaterThan5(5, 5) -> false 
+greaterThan5(1, 4) -> false
+
+*/
+
+// **********************************************************************************************
+                      
+
+ /*                              *** Exercice 4 ***
+
+ function allGreaterThan5(num1,num2){
+
+    if (num1 > 5 && num2 > 5){
+        
+        return true
+    }
+
+else if ((num1 < 5 || num2 < 5) || (num1 < 5 && num2 < 5))
+
+    return false
+    
+}
+
+***invoking***
+
+allGreaterThan5(6, 7) -> true 
+allGreaterThan5(2, 7) -> false
+
+*/
+
+// **********************************************************************************************
+                      
+
+ /*                              *** Exercice 5 ***
+
+ 
+ 
+
+
+***invoking***
+
+
+
+*/
+
+
+
 
 
 
