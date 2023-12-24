3eeb68d062312c95bbc6ab57b5b814f3eb83fd62
walidgzih12 <walidgzih3@gmail.com>
2023-12-24 18:02:18 +0100

    work on progress

--- Functions.js
+++ Functions.js
@@ -108,6 +108,25 @@ else if(x2 > x1){
 largest()---> 10
 
 
+// **********************************************************************************************
+                      
+
+ /*                              *** Exercice 6 ***
+
+ 
+    function add(number1,number2) {
+   
+    return number1+number2
+}
+    var result = add(2,3)
+    console.log("result is : "+ result)
+    
+ 
+***invoking***
+result is : 5
+
+
+
 
 
 
